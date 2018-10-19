c73e3ceeea3bac1690488e6084ddd16d80253d5b
lufangyong <lu1329@qq.com>
2018-10-19 17:24:08 +0800

    add food list

--- vue-sell/src/views/Goods.vue
+++ vue-sell/src/views/Goods.vue
@@ -6,11 +6,44 @@
             v-for="(item, index) in goods"
             :key="index"
             :class="{'current': currentIndex === index}"
-            ref="menuItem"
+            ref="menuList"
+            @click="selectMenu(index,$event)"
         >
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul ref="foodsContent">
+        <li class="food-list"
+            v-for="(item, index) in goods"
+            :key="index"
+            ref="foodsList"
+        >
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li class="food-item border-1px"
+                v-for="(food, index) in item.foods"
+                :key="index"
+            >
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon" alt="icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
         </li>
       </ul>
     </div>
@@ -25,11 +58,25 @@
     data() {
       return {
         goods: [],
+        listHeight: [],
+        scrollY: '',
       }
     },
     components: {},
     computed: {
       currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+
+          if (!height2 || this.scrollY >= height1 && this.scrollY < height2) {
+            console.log('i', i);
+            console.log('this.scrollY', this.scrollY);
+            console.log('this.listHeight', this.listHeight);
+            this._followScroll(i)
+            return i
+          }
+        }
         return 0
       }
     },
@@ -38,25 +85,67 @@
 
       this.getData()
     },
-    mounted() {
-
-    },
     methods: {
       getData() {
         this.axios.get('v1/api/goods').then(res => {
           this.goods = res.data.data.goods
           this.$nextTick(() => {
-            this.__initScroll()
+            this._initScroll()
+            this._calculateHeight()
           })
         }).catch(err => {
           throw new Error(err)
         })
       },
-      __initScroll() {
+      selectMenu(index, evnet) {
+        // better-scroll会将点击事件去掉，如果滚动部分需要有点击事件，需要在参数里加上click：true。
+        // 同时，在PC上或某些手机端，由于未成功将touchend事件move掉，点击事件会执行两次。
+        // better-scroll派发的event事件和原生js的event有属性上的区别，其中有一个属性为event._constructed。better-scroll派发的事件中event._constructed为true，原生点击事件中没有这个属性
+        if (!event._constructed) {
+          return
+        }
+
+        let foodsList = this.$refs.foodsList
+        let el = foodsList[index]
+        this.foodsScroll.scrollToElement(el, 300)
+      },
+      _initScroll() {
         this.menuScroll = new BScroll(this.$refs.menuWrapper, {
           click: true
         })
-      }
+
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3 // 实时派发 scroll 事件
+        })
+
+        this.foodsScroll.on('scroll', (pos) => {
+          // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+          if (pos.y <= 0) {
+            this.scrollY = Math.abs(Math.round(pos.y));
+          }
+        })
+      },
+      // 计算区间高度
+      _calculateHeight() {
+        let foodsList = this.$refs.foodsList
+        foodsList[foodsList.length - 1].style.paddingBottom = '300px'
+        console.log(' foodsList[foodsList.length]',);
+        let height = 0
+        this.listHeight.push(height)
+
+        for (let i = 0; i < foodsList.length; i++) {
+          let item = foodsList[i]
+          height += item.clientHeight
+          this.listHeight.push(height)
+        }
+      },
+      //
+      _followScroll(index) {
+        let menuList = this.$refs.menuList
+        let el = menuList[index]
+        this.menuScroll.scrollToElement(el, 300, 0, -100)
+      },
     }
   }
 </script>
@@ -113,4 +202,58 @@
           vertical-align: middle
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 12px
+    .foods-wrapper
+      flex: 1
+      .title
+        padding-left: 14px
+        height: 26px
+        line-height: 26px
+        border-left: 2px solid #d9dde1
+        font-size: 12px
+        color: rgb(147, 153, 159)
+        background: #f3f5f7
+      .food-item
+        display: flex
+        margin: 18px
+        padding-bottom: 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        &:last-child
+          border-none()
+          margin-bottom: 0
+        .icon
+          flex: 0 0 57px
+          margin-right: 10px
+        .content
+          flex: 1
+          .name
+            margin: 2px 0 8px 0
+            height: 14px
+            line-height: 14px
+            font-size: 14px
+            color: rgb(7, 17, 27)
+          .desc, .extra
+            line-height: 10px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+          .desc
+            line-height: 12px
+            margin-bottom: 8px
+          .extra
+            .count
+              margin-right: 12px
+          .price
+            font-weight: 700
+            line-height: 24px
+            .now
+              margin-right: 8px
+              font-size: 14px
+              color: rgb(240, 20, 20)
+            .old
+              text-decoration: line-through
+              font-size: 10px
+              color: rgb(147, 153, 159)
+          .cartcontrol-wrapper
+            position: absolute
+            right: 0
+            bottom: 12px
 </style>
