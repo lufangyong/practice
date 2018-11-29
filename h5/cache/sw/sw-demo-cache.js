d6d231107a61052570e0e176ef9e99af2d299ed7
lufangyong <lu1329@qq.com>
2018-11-29 09:48:34 +0800

    h5 -> add sw notification

---
+++ h5/cache/sw/sw-demo-cache.js
@@ -0,0 +1,44 @@
+var VERSION = 'v3';
+
+// 缓存
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(VERSION).then(function(cache) {
+      return cache.addAll([
+        './start.html',
+        './static/jquery.min.js',
+        './static/mm.png'
+      ]);
+    })
+  );
+});
+
+// 缓存更新
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          // 如果当前版本和缓存版本不一致
+          if (cacheName !== VERSION) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 捕获请求并返回缓存数据
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).catch(function() {
+    return fetch(event.request);
+  }).then(function(response) {
+    caches.open(VERSION).then(function(cache) {
+      cache.put(event.request, response);
+    });
+    return response.clone();
+  }).catch(function() {
+    return caches.match('./static/mm.png');
+  }));
+});
